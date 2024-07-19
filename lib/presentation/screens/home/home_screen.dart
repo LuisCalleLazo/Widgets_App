aaf5a4534893cbbefa0062927bff51d4cea5ac98
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-07-19 09:52:18 -0400

    Add Go Router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -54,7 +55,8 @@ class _CustomListTitle extends StatelessWidget {
         //     builder: (context) => const ButtonsScreen(),
         //   ),
         // );
-        Navigator.pushNamed(context, menuItems.link);
+        // Navigator.pushNamed(context, menuItems.link);
+        context.push(menuItems.link);
       },
     );
   }
